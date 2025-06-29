85542d8682439d71a8e721219eead9d542aae545
G Vishok <vishokprasad27@gmail.com>
2025-06-29 19:16:57 +0530

    rename interface

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,6 +1,6 @@
 export type Grouping = 'customer_type' | 'industry' | 'acv_range' | 'team';
 
-export interface Data {
+export interface DataSet {
     closed_fiscal_quarter: string;
     count: number;
     acv: number;
