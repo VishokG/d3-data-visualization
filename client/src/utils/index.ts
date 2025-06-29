192080fa7c0cfab89c75be60e5aca10d104308f9
G Vishok <vishokprasad27@gmail.com>
2025-06-29 17:25:02 +0530

    function to format 1000s

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,3 +1,9 @@
+export const shortenCurrency = (val: number) => {
+    if (val >= 1_000) return ("$" + (val / 1_000).toLocaleString(undefined, { maximumFractionDigits: 2 }) + "K");
+    return "$" + val.toLocaleString();
+  }
+  
 export const formatCurrency = (num: number) =>
     num.toLocaleString('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 });
+
 export const formatPercent = (num: number) => `${num}%`;
